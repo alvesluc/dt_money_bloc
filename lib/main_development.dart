7357aac4ca34d0966d31c03f27342a51bccdee96
Lucas Alves <l.alves001@gmail.com>
2023-02-02 00:46:18 -0300

    Structure cleanup

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,13 +1,6 @@
-// Copyright (c) 2023, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
-import 'package:dt_money/app/app.dart';
 import 'package:dt_money/bootstrap.dart';
+import 'package:dt_money/src/app/view/app.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(() => const DtMoneyApp());
 }
