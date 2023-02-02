d4470beea976d3594be55c55093ca88bc6543fcb
Lucas Alves <lucas@vsdi.com.br>
2023-02-01 22:15:35 -0300

    Inital commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.dt_money
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2023, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:dt_money/app/app.dart';
+import 'package:dt_money/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
