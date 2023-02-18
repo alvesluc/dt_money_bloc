62bfccbdcc1bb1ec7a0fee8c4252a5d8071d770e
Lucas Alves <l.alves001@gmail.com>
2023-02-17 22:17:27 -0300

    Added reactivity to dashboard

--- lib/src/home/dashboard/widgets/dashboard_row.dart
+++ lib/src/home/dashboard/widgets/dashboard_row.dart
@@ -1,6 +1,9 @@
 import 'package:dt_money/l10n/l10n.dart';
 import 'package:dt_money/src/home/dashboard/widgets/dashboard_card.dart';
+import 'package:dt_money/src/home/transactions/bloc/transactions_bloc.dart';
+import 'package:dt_money/src/shared/enums.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DashboardRow extends StatelessWidget {
   const DashboardRow({
@@ -15,45 +18,65 @@ class DashboardRow extends StatelessWidget {
     final l10n = context.l10n;
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Row(
-      children: [
-        Expanded(
-          flex: isExpanded ? 1 : 0,
-          child: DashboardCard(
-            title: l10n.dashboardIncomesCardLabel,
-            value: 0,
-            icon: Icon(
-              Icons.arrow_circle_up_rounded,
-              size: 32,
-              color: colorScheme.primary,
+    return BlocBuilder<TransactionsBloc, TransactionsState>(
+      builder: (context, state) {
+        var income = 0.0;
+        var expenses = 0.0;
+
+        if (state.transactions != null && state.transactions!.isNotEmpty) {
+          for (final transaction in state.transactions!) {
+            if (transaction.type == TransactionType.income) {
+              income += transaction.value;
+            }
+            if (transaction.type == TransactionType.expense) {
+              expenses += transaction.value;
+            }
+          }
+        }
+
+        final total = income - expenses;
+
+        return Row(
+          children: [
+            Expanded(
+              flex: isExpanded ? 1 : 0,
+              child: DashboardCard(
+                title: l10n.dashboardIncomesCardLabel,
+                value: income,
+                icon: Icon(
+                  Icons.arrow_circle_up_rounded,
+                  size: 32,
+                  color: colorScheme.primary,
+                ),
+              ),
             ),
-          ),
-        ),
-        Expanded(
-          flex: isExpanded ? 1 : 0,
-          child: DashboardCard(
-            title: l10n.dashboardExpensesCardLabel,
-            value: 0,
-            icon: Icon(
-              Icons.arrow_circle_down_rounded,
-              size: 32,
-              color: colorScheme.error,
+            Expanded(
+              flex: isExpanded ? 1 : 0,
+              child: DashboardCard(
+                title: l10n.dashboardExpensesCardLabel,
+                value: expenses,
+                icon: Icon(
+                  Icons.arrow_circle_down_rounded,
+                  size: 32,
+                  color: colorScheme.error,
+                ),
+              ),
             ),
-          ),
-        ),
-        Expanded(
-          flex: isExpanded ? 1 : 0,
-          child: DashboardCard(
-            title: l10n.dashboardTotalCardLabel,
-            value: 0,
-            icon: Icon(
-              Icons.attach_money_rounded,
-              size: 32,
-              color: colorScheme.onSurface,
+            Expanded(
+              flex: isExpanded ? 1 : 0,
+              child: DashboardCard(
+                title: l10n.dashboardTotalCardLabel,
+                value: total,
+                icon: Icon(
+                  Icons.attach_money_rounded,
+                  size: 32,
+                  color: colorScheme.onSurface,
+                ),
+              ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
